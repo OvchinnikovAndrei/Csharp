de2470ef9cfe6271bf72c623a29093e51208a173
Andrey Ovchinnikov <sunnyhand@bk.ru>
2022-12-01 13:30:37 +0300

    Определение минимального и максимального числа из двух

---
+++ hwSem01/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+int max = a;
+int min = b;
+if (a>b)
+{
+    Console.WriteLine("Максимальное число: "+ a);
+    Console.WriteLine("Минимальное число: " + b);
+}
+else
+{
+    Console.WriteLine("Минимальное число: "+ a);
+    Console.WriteLine("Максимальное число: " + b);  
+}
